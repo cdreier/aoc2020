55837bbd981a4553d68457657d14a9a70e204a61
Christian Dreier <christian.dreier@inovex.de>
2020-12-13 23:52:55 +0100

    day 13 part 1

--- day13/main.exs
+++ day13/main.exs
@@ -1,11 +1,32 @@
 defmodule Day13 do
   def run(file) do
-    list =
+    [target, busses] =
       File.read!(file)
       |> String.split("\n", trim: true)
-      |> Stream.map(&String.graphemes/1)
       |> Enum.to_list()
+
+    busses =
+      String.split(busses, ",")
+      |> Stream.filter(&(&1 != "x"))
+      |> Stream.map(&String.to_integer/1)
+      |> Enum.to_list()
+
+    IO.inspect(busses)
+    target = String.to_integer(target)
+
+    times =
+      busses
+      |> Stream.map(&checkTime(&1, 0, target))
+      |> Enum.to_list()
+      |> IO.inspect()
+
+    {busID, time} = Enum.min_by(times, fn {_, i} -> i end)
+    wait = time - target
+    IO.inspect(busID * wait)
   end
+
+  def checkTime(i, acc, target) when acc >= target, do: {i, acc}
+  def checkTime(i, acc, target), do: checkTime(i, acc + i, target)
 end
 
-Day11.run("test.txt")
+Day13.run("input.txt")
